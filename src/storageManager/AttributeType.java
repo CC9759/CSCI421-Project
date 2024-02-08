be951a5a83bc96d2c6e7511d55f47db444db471d
dvt2706 <dvt2706@rit.edu>
2024-02-08 08:47:13 -0500

    Attribute class

---
+++ src/storageManager/AttributeType.java
@@ -0,0 +1,19 @@
+package storageManager;
+class AttributeType {
+    private enum TYPE {
+        INT, DOUBLE, BOOLEAN, CHAR, VARCHAR
+    }
+
+    public int length; // for char and varchar types. Null on other types
+    public TYPE type;
+
+    public AttributeType(TYPE type) {
+        this.type = type;
+        this.length = -1;
+    }
+
+    public AttributeType(TYPE type, int length) {
+        this.type = type;
+        this.length = length;
+    }
+}
