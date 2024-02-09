94c20b677c47798e5a5fa3853524e1302b6ba14a
dvt2706 <dvt2706@rit.edu>
2024-02-08 19:54:06 -0500

    Defined packages for vscode

--- src/interfaces/StorageManager.java
+++ src/interfaces/StorageManager.java
@@ -7,7 +7,7 @@ public interface StorageManager {
     public Record getRecordByPrimaryKey(Object primaryKey);
     public Page getPage(int tableNumber, int pageNumber);
     public ArrayList<Record> getAllRecords(int tableNumber);
-    public void insertRecord(Record record);
+    public void insertRecord(int tableId, Record record);
     public void deleteRecord(Object PrimaryKey);
     public void updateRecord(Object primaryKey, Record record);
 }
