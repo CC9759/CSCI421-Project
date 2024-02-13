0ae2d6ef03e1326484d16f04e99c7a7a08cf49ae
dvt2706 <dvt2706@rit.edu>
2024-02-13 18:30:23 -0500

    Added a parameter to getRecordByPrimaryKey

--- src/interfaces/StorageManager.java
+++ src/interfaces/StorageManager.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public interface StorageManager {
 
-    public Record getRecordByPrimaryKey(Object primaryKey);
+    public Record getRecordByPrimaryKey(int tableId, Object primaryKey);
     public Page getPage(int tableNumber, int pageNumber);
     public ArrayList<Record> getAllRecords(int tableNumber);
     public void insertRecord(int tableId, Record record);
