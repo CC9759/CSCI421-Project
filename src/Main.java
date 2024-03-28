12a07e04b5e833de1b6cf9f12f99e368dca94779
Celina C <cc9759@rit.edu>
2024-03-28 09:35:34 -0400

    added fix to update and delete parser

--- src/Main.java
+++ src/Main.java
@@ -355,21 +355,24 @@ public class Main {
      * @param commands the string list of commands to process
      */
     public static void updateParser(DMLParser dmlParser, String[] commands) {
+        ArrayList<String> commandsList = new ArrayList<String>(Arrays.asList(commands));
+        int whereIndex = commandsList.indexOf("where");
         String tableName = commands[1];
         String columnName = commands[3];
         String value = commands[5];
-        String whereString;
+        String whereString = null;
 
         if(value.equals("null")){
             value = null;
         }
 
-        if(commands.length > 5){
+        if(whereIndex != -1){
             whereString = String.join(" ", Arrays.copyOfRange(commands, 7, commands.length));
         }
         else{
             whereString = null;
         }
+        
         try{
             dmlParser.update(tableName, columnName, value, whereString);
         } catch(Exception e){
@@ -385,11 +388,13 @@ public class Main {
      * @param commands the string list of commands to process
      */
     public static void deleteParser(DMLParser dmlParser, String[] commands){
+        ArrayList<String> commandsList = new ArrayList<String>(Arrays.asList(commands));
+        int whereIndex = commandsList.indexOf("where");
         String tableName = commands[2];
-        String whereString;
+        String whereString = null;
 
-        if(commands.length > 3){
-            whereString = String.join(" ", Arrays.copyOfRange(commands, 4, commands.length));
+        if(whereIndex != -1){
+            whereString = String.join(" ", Arrays.copyOfRange(commands, whereIndex + 1, commands.length));
         }
         else{
             whereString = null;
