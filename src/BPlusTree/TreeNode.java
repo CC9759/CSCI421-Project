32642c1e758b294bc873c0569d06ad0644876ec4
Norton Andres Perez <nap1118@g.rit.edu>
2024-04-12 23:04:19 -0400

    TreeNode - created divideInternal function to separate internal nodes that get filled up - have a problem with leaf nodes not pointing to each other (in progress) - also found a IndexOutOfBoundsException on one of the tests, need to fix (in progress)

--- src/BPlusTree/TreeNode.java
+++ src/BPlusTree/TreeNode.java
@@ -98,19 +98,74 @@ public class TreeNode {
                 }
                 node.values.clear();
 
-                // separate keys in node to new nodes
-                // TODO: separate keys T_T
-
                 if (node.parent == null) {
                         newNode1.parent = node;
                         newNode1.isLeaf = true;
                         newNode2.parent = node;
                         newNode2.isLeaf = true;
 
+                        newNode1.nextNode = newNode2;
+
                         node.keys.add(newNode1);
                         node.keys.add(newNode2);
                         node.isLeaf = false;
                         node.values.add(newNode2.values.get(0));
+                } else {
+                        newNode1.parent = node.parent;
+                        newNode1.isLeaf = node.isLeaf;
+                        newNode2.parent = node.parent;
+                        newNode2.isLeaf = node.isLeaf;
+
+                        newNode1.nextNode = newNode2;
+                        newNode2.nextNode = node.nextNode;
+
+                        int pointer = node.parent.keys.indexOf(node); // the pointer where new nodes belong
+                        node.parent.keys.remove(node);
+
+                        node.parent.keys.add(pointer, newNode2);
+                        node.parent.keys.add(pointer, newNode1);
+                        if (!node.parent.values.contains(newNode2.values.get(0)))
+                                node.parent.values.add(pointer, newNode2.values.get(0));
+                        else if (!node.parent.values.contains(newNode1.values.get(0)))
+                                node.parent.values.add(pointer, newNode1.values.get(0));
+
+                        if (node.parent.values.size() == this.bucketSize)
+                                divideInternal(node.parent);
+                }
+        }
+
+        private void divideInternal(TreeNode node) {
+                TreeNode newNode1 = new TreeNode(bucketSize);
+                TreeNode newNode2 = new TreeNode(bucketSize);
+
+                int lowestNodeSize = (int) Math.ceil(node.values.size() / 2.0);
+                for (int i = 0; i < lowestNodeSize - 1; i++) {
+                        // copy keys and values in newNode1
+                        newNode1.values.add(node.values.remove(0));
+                }
+                for (int i = 0; i < lowestNodeSize - 1; i++) {
+                        // copy remaining keys and values in newNode2
+                        newNode2.values.add(node.values.remove(1));
+                }
+
+                // give keys to nodes
+                int split = node.keys.size() / 2; // split the pointers
+                for (int i = 0; i < split; i++) {
+                        newNode1.keys.add(node.keys.remove(0));
+                }
+                for (int i = 0; i < split; i++) {
+                        newNode2.keys.add(node.keys.remove(0));
+                }
+
+                if (node.parent == null) {
+                        newNode1.parent = node;
+                        newNode1.isLeaf = false;
+                        newNode2.parent = node;
+                        newNode2.isLeaf = false;
+
+                        node.keys.add(newNode1);
+                        node.keys.add(newNode2);
+                        node.isLeaf = false;
                 } else {
                         newNode1.parent = node.parent;
                         newNode1.isLeaf = node.isLeaf;
@@ -128,7 +183,7 @@ public class TreeNode {
                                 node.parent.values.add(pointer, newNode1.values.get(0));
 
                         if (node.parent.values.size() == this.bucketSize)
-                                divideNode(node.parent);
+                                divideInternal(node.parent);
                 }
         }
 
